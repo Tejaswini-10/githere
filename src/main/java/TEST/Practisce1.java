84350b9fc91e44703a5552af78cb5ae5e3c599c9
Tejaswini-10 <103121648+Tejaswini-10@users.noreply.github.com>
2022-07-25 15:38:59 +0530

    Update Practisce1.java

--- src/main/java/TEST/Practisce1.java
+++ src/main/java/TEST/Practisce1.java
@@ -4,7 +4,7 @@ public class Practisce1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		System.out.println("hello worldd");
 
 	}
 
